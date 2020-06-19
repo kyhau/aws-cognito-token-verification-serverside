77e3e49e5ae3b14eb8517f1e8193b130d089cb59
Kay Hau <virtualda@gmail.com>
2020-06-19 20:35:48 +1000

    Minor maintenance: support building and testing with python 3.7 and 3.8 (#4)
    * Minor maintenance: support testing with python 3.7/3.8

--- ReleaseNotes.md
+++ ReleaseNotes.md
@@ -1,6 +1,12 @@
+Next Release
+==================
+
+- Support building/testing with Python 3.7, 3.8
+
+
 0.1.0 - 2017-10-01
 ==================
 
-Perform the token validation steps as per
-https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
+- Perform the token validation steps as per
+  https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-with-identity-providers.html
 
--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
-from __future__ import absolute_import, division, print_function
 from setuptools import setup, find_packages
 import os
 
 
-# Makes setup work inside of a virtualenv
-use_system_lib = True
-if os.environ.get("BUILD_LIB") == "1":
-    use_system_lib = False
-
-
 base_dir = os.path.dirname(__file__)
-
 __author__ = "Kay Hau"
 __email__ = "virtualda@gmail.com"
-
 __title__ = "cognitoauth"
 __version__ = "0.1.0"
 __summary__ = "This package creates a framework for python packages to be built."
@@ -26,25 +17,42 @@ __requirements__ = [
     "requests[security]"
 ]
 
-with open(os.path.join(base_dir, "README.md")) as f:
-    long_description = f.read()
+__long_description__ = ""
+try:
+    # Reformat description as PyPi use ReStructuredText rather than Markdown
+    import m2r
+
+    __long_description__ = m2r.parse_from_file(os.path.join(base_dir, "README.md"))
+except (ImportError, IOError, OSError) as e:
+    import logging
+
+    logging.warning(f"m2r conversion failed: {e}")
+
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3 :: Only",
+]
 
 setup(
-    name=__title__,
-    version=__version__,
-    description=__summary__,
-    long_description=long_description,
-    packages=find_packages(exclude=["tests"]),
     author=__author__,
     author_email=__email__,
-    url=__uri__,
-    zip_safe=False,
+    classifiers=CLASSIFIERS,
+    # data_files parameter is only required for files outside the packages, used in conjunction with the MANIFEST.in
+    data_files=[("", ["ReleaseNotes.md"]),],
+    description=__summary__,
     install_requires=__requirements__,
-    data_files=[
-        ("", ["ReleaseNotes.md"]),
-    ],
+    long_description=__long_description__,
+    name=__title__,
     # For data inside packages can use the automatic inclusion
-    # include_package_data = True,
-    # or the explicit inclusion, eg:
-    # package_data = { "package_name": ["data.file1", "data.file2" , ...] }
+    #   include_package_data = True,
+    # or the explicit inclusion, e.g.:
+    #   package_data={ "package_name": ["data.file1", "data.file2" , ...] }
+    packages=find_packages(exclude=["tests"]),
+    url=__uri__,
+    version=__version__,
+    zip_safe=False,
 )
